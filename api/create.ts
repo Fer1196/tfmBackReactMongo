4b96fd9f7b5afd126f15b8f8b50f59cabfa7a234
Fer1196 <rodifer1196@hotmail.com>
2024-11-03 16:10:35 -0500

    Add files via upload

---
+++ api/create.ts
@@ -0,0 +1,19 @@
+// api/create.ts
+
+import { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  if (req.method === "POST") {
+    const data = req.body;
+    // Lógica para crear un recurso (ej. guardar en una base de datos)
+    res
+      .status(201)
+      .json({
+        message:
+          "Recurso creado Vercel modificado ahora mismo con nueva data V2",
+        data,
+      });
+  } else {
+    res.status(405).json({ message: "Método no permitido" });
+  }
+};
